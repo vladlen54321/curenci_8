03745b79322fb16163a6e18a1ad0c3adbd5f5090
vladlen54321 <vladred54321@gmail.com>
2023-04-02 11:38:45 +0300

    vtj

--- app/currency/utils.py
+++ app/currency/utils.py
@@ -5,9 +5,9 @@ import string
 def get_password(length: int = 10) -> str:
 
     password = ''
-    cgars = string.ascii_letters + string.digits + string.punctuation
+    cgars =string.ascii_letters + string.digits + string.punctuation
     for _ in range(length):
 
         password += random.choice(cgars)
-        
+
     return password
